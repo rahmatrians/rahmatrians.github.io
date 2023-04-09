1ca68de71f7e30de3d2959d0f4f1f0d47003b25b
Rahmat Riansyah <rahmatriansyah57@gmail.com>
2023-04-09 12:41:12 +0700

    first fly⚡

---
+++ _nuxt/c9b7cc0.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{332:function(n,t,e){"use strict";e.r(t);var o=e(43),component=Object(o.a)({},(function(){return(0,this._self._c)("h1",[this._v("Yoo! dude")])}),[],!1,null,null,null);t.default=component.exports}}]);
